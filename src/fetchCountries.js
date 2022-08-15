6679cc3d3e0bdef53c2bd28f25004bf257b7b818
shuriuss <104681622+shuriuss@users.noreply.github.com>
2022-08-15 20:16:01 +0300

    update

---
+++ src/fetchCountries.js
@@ -0,0 +1,15 @@
+export function fetchCountries(searchContry) {
+    if(!searchContry){
+        return
+    }
+
+  return fetch(`https://restcountries.com/v3.1/name/${searchContry}?fields=name,capital,population,flags,languages`)
+    .then(r => {
+      if (!r.ok) {
+        throw new Error(r.status);
+      }
+      return r.json();
+    })
+    
+   
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,65 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './fetchCountries';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
+
+const countryEl = document.querySelector('#search-box');
+countryEl.addEventListener('input', debounce(handler, DEBOUNCE_DELAY));
+
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+function handler(e) {
+  e.preventDefault();
+  clear();
+  const searchContry = e.target.value;
+  console.log(searchContry);
+  // console.log(searchContry);
+  fetchCountries(searchContry)
+    .then(data => renderUserList(data))
+    .catch(error => {
+      if(error= 404){
+        Notify.failure("Oops, there is no country with that name");
+      }
+            console.log(error)});
+
+}
+
+function renderUserList(data) {
+  if (data.length > 10) {
+    Notify.info('Too many matches found. Please enter a more specific name.');
+    countryList.innerHTML = '';
+    return;
+  }
+
+  if (data.length > 1 && data.length < 10) {
+    const markup = data
+      .map(({ name, flags }) => {
+        return ` <li><img  class='img' src='${flags.svg}' alt='flag of ${name.official}'/>  ${name.official} </li>`;
+      })
+      .join('');
+    countryList.innerHTML = markup;
+    return;
+  }
+
+  if (data.length === 1) {
+    const markup = data
+      .map(({ name, flags, capital, population, languages }) => {
+        return `<img  class='img-info' src='${flags.svg}' alt='flag of ${name.official}'/>
+        <p class="country">${name.official}</p>
+        <p class='info'><span>Capital: </span>${capital}</p>
+        <p class='info'><span>Population: </span>${population}</p>
+        <p class='info'><span>Languages: </span>${languages}</p>`;
+      })
+      .join('');
+    countryInfo.innerHTML = markup;
+    return;
+  }
+}
+
+function clear() {
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
+}
